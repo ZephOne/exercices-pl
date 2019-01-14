c5b26ad281b32340b46bdf8362ea15e6868960b4
ZephOne <zephone@protonmail.com>
2019-01-14 15:38:42 +0100

    Dealt with cases $a and $b equal 0

--- exercice_10.pl
+++ exercice_10.pl
@@ -2,33 +2,67 @@
 use strict;
 use utf8;
 use Math::Complex;
-binmode(STDOUT, ":utf8");
+use Switch;
 
 print "For an equation like this: ax² + bx + c = 0\n";
 print "Give me a:\n";
 my $a = <STDIN>;
 chomp($a);
+
+while(!($a =~ /^-?[0-9]+$/)) {
+  print "Oh no! You didn't give me an integer for a!!!";
+  print "Give me an integer for a:\n";
+  $a = <STDIN>;
+  chomp($a);
+}
+
 print "Give me b:\n";
 my $b = <STDIN>;
 chomp($b);
+
+while(!($b =~ /^-?[0-9]+$/)) {
+  print "Oh no! You didn't give me an integer for b!!!";
+  print "Give me an integer for b:\n";
+  $b = <STDIN>;
+  chomp($b);
+}
+
 print "Give me c:\n";
 my $c = <STDIN>;
 chomp($c);
 
-my $Δ = $b*$b - 4*$a*$c;
+while(!($c =~ /^-?[0-9]+$/)) {
+  print "Oh no! You didn't give me an integer for c!!!";
+  print "Give me an integer for c:\n";
+  $c = <STDIN>;
+  chomp($c);
+}
 
-if ($Δ == 0) {
-  my $r = -$b/(2*$a);
-  print "The double solution of your equation is $r";
-} else {
-  my $r1;
-  my $r2;
-  if ($Δ > 0) {
-    $r1 = ($b*$b + sqrt($Δ))/(2*$a);
-    $r2 = ($b*$b - sqrt($Δ))/(2*$a);
+
+if ($a == 0) {
+  if ($b == 0) {
+    print "$c = 0, Nobel prize!!!\n";
   } else {
-    $r1 = ($b*$b + i*sqrt(-$Δ))/(2*$a);
-    $r2 = ($b*$b - i*sqrt(-$Δ))/(2*$a);
+    print "The solution of your equation is ", (-$c/$b), "\n";
   }
-  print "The solutions of your equation are $r1 and $r2";
+} else {
+
+  my $Δ = $b*$b - 4*$a*$c;
+
+  switch($Δ) {
+    case 0 {
+      my $r = -$b/(2*$a);
+      print "The double solution of your equation is $r";
+    }
+    case { $_[0] > 0 } {
+      my $r1 = ($b*$b + sqrt($Δ))/(2*$a);
+      my $r2 = ($b*$b - sqrt($Δ))/(2*$a);
+      print "The solutions of your equation are $r1 and $r2";
+    }
+    case  { $_[0] < 0 } {
+      my $r1 = ($b*$b + i*sqrt(-$Δ))/(2*$a);
+      my $r2 = ($b*$b - i*sqrt(-$Δ))/(2*$a);
+      print "The solutions of your equation are $r1 and $r2";
+    }
+  }
 }
